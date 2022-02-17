b7796371dd05c04430eaa9ea0222417e7f4920ed
ab-morphious <ab.daabsoft@gmail.com>
2022-02-17 15:29:57 +0300

    added splash successfully

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_blue_elves/flutter_blue_elves.dart';
 import 'package:path_provider/path_provider.dart';
-import 'device_control.dart';
+import 'pages/device_control.dart';
 import 'dart:io';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
@@ -13,7 +13,7 @@ void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MaterialApp(home: MyApp()));
-
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatefulWidget {
@@ -40,7 +40,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    FlutterNativeSplash.remove();
   }
 
   List<DataRow> _rowList = [];
