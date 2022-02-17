88b740af00f8e9bef92c8e12e72464935cbb2d09
ab-morphious <ab.daabsoft@gmail.com>
2022-02-17 16:51:47 +0300

    good state

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,7 +13,6 @@ void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MaterialApp(home: MyApp()));
-  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatefulWidget {
@@ -40,6 +39,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    FlutterNativeSplash.remove();
   }
 
   List<DataRow> _rowList = [];
